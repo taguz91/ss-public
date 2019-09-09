410cea8b2145f241e1f972d4eed85a70cae5eadf
Johnny <johnnygar98@hotmail.com>
2019-09-09 15:31:59 -0500

    Agregando indicaciones y solucinando un error de los imports

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 FrondEnd de ShopShop para clientes y vendedores. 
 
+## Indicaciones  
+
+### Components
+
+- Todos los components que terminan en "form" son para crear los formularios, metodos ingresar y editar. 
+- Todos los components que terminan en "list" sirven para enlistar lo correspondiente, podrias agregar filtros, o un buscador, al inicio usaremos solo tablas pero luego el diseño cambiara por algo mejor. 
+- Todos los components que unicamente tienen el nombre del objeto son para verlo por individual, como toda la informacion, del mismo en casos especiales tendran todo lo que corresponde, siendo ingreso eliminacion y listado, por ejemplo en "comentario".
+
+### Servicios
+
+
+
+##Entregables 
+
 ### Primer Entregable  
 
 - Registro de Vendedores 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductoComponent } from './components/producto/producto.component';
-import { VendedorComponent } from './components/vendedor/vendedor.component';
 import { LoginComponent } from './components/login/login.component';
-import { ClienteComponent } from './components/cliente/cliente.component';
-import { LocalComponent } from './components/local/local.component';
-import { ComentarioComponent } from './components/comentario/comentario.component';
 import { ProductoListComponent } from './components/producto-ss/producto/producto-list/producto-list.component';
 import { ClienteListComponent } from './components/human-ss/cliente/cliente-list/cliente-list.component';
 import { LocalListComponent } from './components/human-ss/vendedor/local-list/local-list.component';
 import { VendedorFormComponent } from './components/human-ss/vendedor/vendedor-form/vendedor-form.component';
 import { LocalFormComponent } from './components/human-ss/vendedor/local-form/local-form.component';
-import { ProcutoFormComponent } from './components/producto-ss/producto/procuto-form/procuto-form.component';
 import { ProductoFormComponent } from './components/producto-ss/producto/producto-form/producto-form.component';
 import { ClienteFormComponent } from './components/human-ss/cliente/cliente-form/cliente-form.component';
+import { ProductoComponent } from './components/producto-ss/producto/producto/producto.component';
+import { VendedorComponent } from './components/human-ss/vendedor/vendedor/vendedor.component';
+import { ClienteComponent } from './components/human-ss/cliente/cliente/cliente.component';
+import { LocalComponent } from './components/human-ss/vendedor/local/local.component';
+import { ComentarioComponent } from './components/producto-ss/comentario/comentario/comentario.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +31,6 @@ import { ClienteFormComponent } from './components/human-ss/cliente/cliente-form
     LocalListComponent,
     VendedorFormComponent,
     LocalFormComponent,
-    ProcutoFormComponent,
     ProductoFormComponent,
     ClienteFormComponent
   ],
