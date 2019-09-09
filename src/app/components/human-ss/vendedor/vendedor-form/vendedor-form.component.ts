a11aa0ad000a04cd00e9473502737d745b55f4c2
Johnny <johnnygar98@hotmail.com>
2019-09-09 15:24:12 -0500

    Agregando todos los componentes

---
+++ src/app/components/human-ss/vendedor/vendedor-form/vendedor-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendedor-form',
+  templateUrl: './vendedor-form.component.html',
+  styleUrls: ['./vendedor-form.component.css']
+})
+export class VendedorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
