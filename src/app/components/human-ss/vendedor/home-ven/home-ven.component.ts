32acfe1c3723a7e9d8d4ea141d2dcd8793f66e0e
Gustavo <johnnygar98@hotmail.com>
2019-10-07 11:51:32 -0500

    Arreglando la navegacion

---
+++ src/app/components/human-ss/vendedor/home-ven/home-ven.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-ven',
+  templateUrl: './home-ven.component.html',
+  styleUrls: ['./home-ven.component.css']
+})
+export class HomeVenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
